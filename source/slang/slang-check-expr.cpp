70bac1aad321f233f57744aa16841c0b3fc4bee3
Aidan Foster <aidanf@nvidia.com>
2026-02-27 16:24:04 -0800

    remove temporary error diagnostic, too much spam from confused LLMs

--- source/slang/slang-check-expr.cpp
+++ source/slang/slang-check-expr.cpp
@@ -3003,24 +3003,6 @@ Expr* SemanticsVisitor::CheckInvokeExprWithCheckedOperands(InvokeExpr* expr)
             if (funcDeclRefExpr)
                 funcDeclBase = as<FunctionDeclBase>(funcDeclRefExpr->declRef.getDecl());
 
-            if (funcDeclRefExpr)
-            {
-                auto knownBuiltinAttrDeclRef = getDeclRef(m_astBuilder, funcDeclRefExpr)
-                                                    .getDecl()
-                                                    ->findModifier<KnownBuiltinAttribute>();
-                if (auto knownBuiltinAttr = as<KnownBuiltinAttribute>(knownBuiltinAttrDeclRef))
-                {
-                    if (auto constantIntVal = as<ConstantIntVal>(knownBuiltinAttr->name))
-                    {
-                        if (constantIntVal->getValue() ==
-                            (int)KnownBuiltinDeclName::OperatorAddressOf)
-                        {
-                            getSink()->diagnose(invoke, Diagnostics::invalidAddressOf);
-                        }
-                    }
-                }
-            }
-
             Index paramCount = funcType->getParamCount();
             for (Index pp = 0; pp < paramCount; ++pp)
             {
